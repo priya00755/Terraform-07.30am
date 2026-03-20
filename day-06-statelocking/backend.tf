9cd7883606ef989886d7d44e8d29ffddef14568b
priya <priyaagawane007@gmail.com>
2026-03-20 19:22:30 +0530

    statelock

--- day-06-statelocking/backend.tf
+++ day-06-statelocking/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "piya-s3-tf-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true #dynmodb no longer required for state locking in s3 backend we can use lockfile for state locking in s3 bucket
+     # terraform version should be 1.10 above to use lockfile for state locking in s3 bucket
   }
 }
